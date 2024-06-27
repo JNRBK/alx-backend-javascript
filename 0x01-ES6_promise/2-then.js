51dec6d8e8097a5d82f909389160308c25e965d0
JNRBK <rafeekmorgan@yahoo.com>
2024-06-27 04:24:19 +0300

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     if (promise) {
       resolve({ status: 200, body: 'success' });
-      console.log('Got a response from the API');
+      // console.log('Got a response from the API');
     } else {
       reject();
     }
-  });
+  })
+    .then(() => console.log('Got a response from the API'));
 }
-// () => console.log('Got a response from the API')
