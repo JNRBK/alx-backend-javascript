e7f96b9a4a2f5fbb4ffbdbb0e437dd0203b96b00
JNRBK <rafeekmorgan@yahoo.com>
2024-07-01 01:35:50 +0300

    first for -- of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
-/* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+  const NewArr = [];
+  for (const idx of array) {
+    NewArr.push(appendString + idx);
   }
-  return array;
+  return NewArr;
 }
